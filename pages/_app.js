34aab8b499794c3698030241937eb5a854a71ad0
eyuel <eyuelket2019@gmail.com>
2022-09-03 20:21:43 +0300

    day four - player ui

--- pages/_app.js
+++ pages/_app.js
@@ -3,14 +3,15 @@ import Header from "../components/Header"
 import { ChakraProvider } from "@chakra-ui/react"
 import SideBar from "../components/SideBar"
 import RightPanel from "../components/RightPanel"
-
+import Player from "../components/Player"
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <Header />
       <SideBar/>
       <Component {...pageProps} />
-      <RightPanel/>
+      <RightPanel />
+      <Player/>
     </ChakraProvider>
   )
 }
